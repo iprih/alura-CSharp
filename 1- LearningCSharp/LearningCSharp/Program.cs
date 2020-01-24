9e0af4cad8f79c591d510a463a220ce2d21946f8
iprih <priscila.silva076@gmail.com>
2020-01-23 23:31:32 -0300

    initial commit

---
+++ 1- LearningCSharp/LearningCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo! Iniciando os estudos CSharp!");
+            Console.WriteLine("Tecle enter para finalizar a aplicação...");
+            Console.ReadLine();
+        }
+    }
+}
