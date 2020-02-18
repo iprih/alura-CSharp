317e831ddcceb570c930aa90ac580b7ff12b0741
iprih <priscila.silva076@gmail.com>
2020-02-18 14:57:44 -0300

    atualizando

---
+++ 1- LearningCSharp/Condicionais/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 16;
+            int qtdPessoas = 1;
+
+            if (idade >= 18 || qtdPessoas >=2)
+            {
+                Console.WriteLine("Pode entrar");
+            }
+            else
+            {
+                Console.WriteLine("Nao pode entrar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
