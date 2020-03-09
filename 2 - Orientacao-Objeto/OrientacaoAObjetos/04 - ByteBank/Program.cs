7e04ca3690d35b4eaf1815881b7167615a8f5041
iprih <priscila.silva076@gmail.com>
2020-03-09 14:22:25 -0300

    3 e inicio do 4 modulo

--- 2 - Orientacao-Objeto/OrientacaoAObjetos/04 - ByteBank/Program.cs
+++ 2 - Orientacao-Objeto/OrientacaoAObjetos/04 - ByteBank/Program.cs
@@ -13,6 +13,27 @@ namespace _04___ByteBank
             Console.WriteLine("Após o saque o saldo é: " + contaPriscila.saldo);
             Console.WriteLine(resultadoSaque);
 
+            contaPriscila.Depositar(500);
+            Console.WriteLine(contaPriscila.saldo);
+
+            ContaCorrente contaRogerio = new ContaCorrente();
+
+            contaRogerio.titular = "Rogerio";
+
+            Console.WriteLine("Saldo Pri :" + contaPriscila.saldo);
+            Console.WriteLine("Saldo Rogerio :" + contaRogerio.saldo);
+
+            bool resultadoTransferencia = contaPriscila.Transferir(200, contaRogerio);
+
+            Console.WriteLine("Saldo Pri: " + contaPriscila.saldo);
+            Console.WriteLine("Saldo Rogerio: " + contaRogerio.saldo);
+            Console.WriteLine(resultadoTransferencia);
+
+            contaRogerio.Transferir(100, contaPriscila);
+
+            Console.WriteLine("Saldo Pri: " + contaPriscila.saldo);
+            Console.WriteLine("Saldo Rogerio: " + contaRogerio.saldo);
+            Console.WriteLine(resultadoTransferencia);
 
         }
     }
---
+++ 2 - Orientacao-Objeto/OrientacaoAObjetos/05 - ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente priscila = new Cliente();
+
+            //priscila.nome = "Priscila";
+            //priscila.profissao = "Desenvolvedora";
+            //priscila.cpf = "123456789";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            //conta.titular = priscila;
+            conta.titular = new Cliente();
+            conta.titular.nome = "Priscila Silva";
+            conta.titular.cpf = "123456789";
+            conta.titular.profissao = "Desenvolvedora";
+            conta.saldo = 500;
+            conta.agencia = 456;
+            conta.numero = 12345;
+
+           //Console.WriteLine(priscila.nome);
+
+            
+           
+            Console.WriteLine(conta.titular.nome + ", " + conta.titular.profissao);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
